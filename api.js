be94789316834a59e84416aa65964e438f7c5d26
Chrystian de Matos <cristianlindo02@gmail.com>
2021-02-05 16:21:34 -0300

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const backend = require('./backend.js');
 
-app.get('/api/teste', async (req, res) => {
+app.get('/', async (req, res) => {
     let resp = await backend.getServers();
     res.send(resp);
 })
